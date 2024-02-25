ebdb9e5678aa56d991051a8bbbe6c817161443db
Henzz <khenok53@gmail.com>
2024-02-26 00:45:11 +0300

    fix

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -12,11 +12,11 @@ def fizzbuzz(n):
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
-    if n < 0:
+    if n < 1:
         return
 
     tmp_result = []
-    for i in range(1, n):
+    for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
