c0866d9e03f495d3093ed2ce4aebf9e9f3c543ef
Henzz <khenok53@gmail.com>
2024-02-26 01:29:42 +0300

    4 - fix

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -44,7 +44,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		tmp = (*head)->prev;
 		tmp->next = (*head)->next;
-		(*head)->prev->prev = (*head)->prev;
 		if ((*head)->next)
 			(*head)->next->prev = tmp;
 		free(*head);
